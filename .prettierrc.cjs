f91d74257ade9048be233513958d8655c48e00f7
Diego C. Silva <diegocaetano444@outlook.com>
2024-05-25 02:08:45 -0300

    chore(lint): configure lint

---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Options} */
+module.exports = {
+    plugins: ['prettier-plugin-tailwindcss'],
+    trailingComma: 'none',
+    semi: true,
+    singleQuote: true,
+    tabWidth: 2,
+    arrowParens: 'avoid'
+}
