343b0ace08e86fbfddf3d8a34f27194df7635921
Diego C. Silva <diegocaetano444@outlook.com>
2024-07-29 19:54:06 -0300

    Update braind.ts

--- content/braind.ts
+++ content/braind.ts
@@ -1,11 +1,11 @@
 export const braindInfo = {
   site_url: '',
-  braind_name: 'Glossier',
-  last_update: 'Aug 2024',
-  website: 'https://sephora.com',
-  industry: 'Beauty',
-  sub_industry: 'Cosmetics',
-  logo: 'https://www.google.com/s2/favicons?domain=sephora.com&sz=128',
+  braind_name: '',
+  last_update: '',
+  website: '',
+  industry: '',
+  sub_industry: '',
+  logo: '',
   description:
-    'Wide selection of beauty products and cosmetics, from makeup to skincare, for all your beauty needs. Offers top brands, expert advice, and personalized recommendations both online and in stores.'
+    ''
 };
